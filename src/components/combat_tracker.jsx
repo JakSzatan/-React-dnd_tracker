6883fe8090b3b670b0e8fc6e79ce267445e2891a
JakSzatan <47736299+JakSzatan@users.noreply.github.com>
2022-01-30 17:48:23 +0100

    Clean up in combat_tracker

--- src/components/combat_tracker.jsx
+++ src/components/combat_tracker.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import MonsterEntry from "./monster_entry";
-import Multiselect from 'multiselect-react-dropdown';
-import RollEntry from './roll_entry'
 import SaveModal  from "./savemodal";
 import Rolling_tray from "./rolling_tray";
-
+import Multi_roll from "./multi_roll";
 export default class CombatTracker extends React.Component {
 
 
@@ -19,6 +17,7 @@ export default class CombatTracker extends React.Component {
         Disadvantage:false,
         MonserList:[]
     };
+      this.handler=this.handler.bind(this);
       this.handleChange = this.handleChange.bind(this);
       this.Save =this.Save.bind(this)
       this.roll =this.roll.bind(this)
@@ -63,23 +62,25 @@ export default class CombatTracker extends React.Component {
       this.multiselectRef.current.resetSelectedValues();
     }
 
-    multiRoll(dice){
+    multiRoll(dice,state,handler){
       ///Rolls a random value for selected items
       let arraySel = this.multiselectRef.current.getSelectedItems();
       let resultarray = arraySel.map(a => a.value.item);
-      var array = [...this.state.EnemyList];
-      if (this.state.Advatage) {
+      var array = [...state.EnemyList];
+      if (state.Advatage) {
         resultarray.map(a=>a.rolledVal=(Math.max(Math.floor(Math.random() * (dice - 1 + 1)) + 1,Math.floor(Math.random() * (dice - 1 + 1)) + 1)+parseFloat(this.state.modifier)));
       }
-      else if (this.state.Disadvantage) {
+      else if (state.Disadvantage) {
         resultarray.map(a=>a.rolledVal=(Math.min(Math.floor(Math.random() * (dice - 1 + 1)) + 1,Math.floor(Math.random() * (dice - 1 + 1)) + 1)+parseFloat(this.state.modifier)));
       }
       else{
         resultarray.map(a=>a.rolledVal=(Math.floor(Math.random() * (dice - 1 + 1)) + 1)+parseFloat(this.state.modifier));
       }
+      handler(array)
+    }
+    handler(array){
       this.setState({EnemyList:array})
     }
-
     roll(item,dice){
       let Result= Math.floor(Math.random() * (dice - 1 + 1)) + 1;
       let Result2= Math.floor(Math.random() * (dice - 1 + 1)) + 1;
@@ -126,22 +127,7 @@ export default class CombatTracker extends React.Component {
       else{
         this.setState({Advatage:false,Disadvantage:false})
       }
-      
-  
     }
-  //  SelectAll(array){
-  //   let old = localStorage.getItem('Selected');
-  //   let intersection = array.filter(x => !old.includes(x));
-  //   localStorage.setItem('Selected', array);
-  //   console.log(intersection)
-  //   }
-  //   DeslectAll(array){
-  //   let old = localStorage.getItem('Selected');
-  //   let intersection = array.filter(x => !old.includes(x));
-  //   localStorage.setItem('Selected', array);
-  //   console.log(intersection)
-  //   }
-    
 
     render() {
 
@@ -155,72 +141,38 @@ export default class CombatTracker extends React.Component {
         <div className="flex-row d-flex flex-wrap">
 
           <div className="flex-row col-6 ps-3 pe-3 mt-5">
-            <input type="text" list="monstername" placeholder="name" name="name" onChange={this.handleChange} className="input-group-text input-z-index d-inline col-4"/>
-            
 
+            <input type="text" list="monstername" placeholder="name" name="name" onChange={this.handleChange} className="input-group-text input-z-index d-inline col-4"/>
             <datalist id="monstername">
               {this.state.MonserList.map(a=><option key={a.name} value={a.name}/>)}
             </datalist>
-
-
+            
             <input type="text" placeholder="AC" name="ac"onChange={this.handleChange} className="input-group-text input-z-index d-inline col-1"/>
             <input type="text" placeholder="HP" name="hp" onChange={this.handleChange} className="input-group-text input-z-index d-inline col-1"/>
             <button type="button" onClick={()=>this.add(this.state.name,this.state.hp,this.state.ac)} className="btn btn-success align-baseline col-1 bi bi-plus" ></button>
+            
             <div className="mt-2">
+
             {this.state.EnemyList.map((item,index)=>
                 <MonsterEntry key={item.id} data={item} delete={()=>this.delete(index)}/>
             )}
             </div>
-          </div>
-          <div className="col-6 border-start border-dark ps-3 pe-3">
-          <div className="col-12 fs-3">Multi Roll</div>
-          <div className="col-12">
-
-          <div onChange={this.onChangeValue}>
-
-          <input type="radio" className="option-input radio" value="Advatage" name="Advatage" />Advatage
-
-          <input type="radio" className="option-input radio" value="Disadvantage" name="Advatage" />Disadvantage
-
-          <input type="radio" className="option-input radio" value="None" name="Advatage" />None
 
           </div>
-            
-          </div>
-          <div className="col-12 flex-row d-flex">
-          <input type="text" placeholder="modifier" name="modifier" onChange={this.handleChange} className="input-group-text input-z-index d-inline col-2"/>
-                <div className="col-4">
-                    <Multiselect
-                    ref={this.multiselectRef}
-                    options={options}
-                    displayValue="label"
-                    closeOnSelect={false}
-                    showCheckbox
-                    style={{
-                        chips: {
-                          display:"none"
-                        },
-                        inputField: { 
-                            width:"100%"
-                        }
-                      }}
-                    showArrow
-                    />
-                </div>
-                <button type="button" onClick={()=>{this.multiRoll(20)}} className="btn btn-success align-baseline col-1" >d20</button>
-                <button type="button" onClick={()=>{this.multiRoll(12)}} className="btn btn-success align-baseline col-1" >d12</button>
-                <button type="button" onClick={()=>{this.multiRoll(10)}} className="btn btn-success align-baseline col-1" >d10</button>
-                <button type="button" onClick={()=>{this.multiRoll(8)}} className="btn btn-success align-baseline col-1" >d8</button>
-                <button type="button" onClick={()=>{this.multiRoll(6)}} className="btn btn-success align-baseline col-1" >d6</button>
-                <button type="button" onClick={()=>{this.multiRoll(4)}} className="btn btn-success align-baseline col-1" >d4</button>
-                </div>
-                <div className="mt-3"></div>
-                {this.state.EnemyList.map((item,index)=>
-                  <RollEntry key={item.id} data={item} roll={this.roll}/>
-                )}
-          </div>
+
+              <Multi_roll
+              onChangeValue={this.onChangeValue}
+              handleChange={this.handleChange}
+              options={options}
+              EnemyList={this.state.EnemyList}
+              multiRoll={this.multiRoll}
+              multiselectRef={this.multiselectRef}
+              roll={this.roll}
+              state={this.state}
+              handler={this.handler}
+                 />
+
                   <SaveModal Save={this.Save}/>
-
         {this.state.presetList.map((item,index)=>
                   <button key={"a"+index} className="mt-5 btn btn-info text-center" onClick={()=>this.Load(item)}>
                           {item}
---
+++ src/components/multi_roll.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Multiselect from 'multiselect-react-dropdown';
+import RollEntry from './roll_entry'
+
+class multi_roll extends Component {
+    render() {
+        return (
+            <div className="col-6 border-start border-dark ps-3 pe-3">
+            <div className="col-12 fs-3">Multi Roll</div>
+            <div className="col-12">
+  
+            <div onChange={this.props.onChangeValue}>
+  
+            <input type="radio" className="option-input radio" value="Advatage" name="Advatage" />Advatage
+            <input type="radio" className="option-input radio" value="Disadvantage" name="Advatage" />Disadvantage
+            <input type="radio" className="option-input radio" value="None" name="Advatage" />None
+  
+            </div>
+              
+            </div>
+            <div className="col-12 flex-row d-flex">
+            <input type="text" placeholder="modifier" name="modifier" onChange={this.props.handleChange} className="input-group-text input-z-index d-inline col-2"/>
+                  <div className="col-4">
+                      <Multiselect
+                      ref={this.props.multiselectRef}
+                      options={this.props.options}
+                      displayValue="label"
+                      closeOnSelect={false}
+                      showCheckbox
+                      style={{
+                          chips: {
+                            display:"none"
+                          },
+                          inputField: { 
+                              width:"100%"
+                          }
+                        }}
+                      showArrow
+                      />
+                  </div>
+                  <button type="button" onClick={()=>{this.props.multiRoll(20,this.props.state,this.props.handler)}} className="btn btn-success align-baseline col-1" >d20</button>
+                  <button type="button" onClick={()=>{this.props.multiRoll(12,this.props.state,this.props.handler)}} className="btn btn-success align-baseline col-1" >d12</button>
+                  <button type="button" onClick={()=>{this.props.multiRoll(10,this.props.state,this.props.handler)}} className="btn btn-success align-baseline col-1" >d10</button>
+                  <button type="button" onClick={()=>{this.props.multiRoll(8,this.props.state,this.props.handler)}} className="btn btn-success align-baseline col-1" >d8</button>
+                  <button type="button" onClick={()=>{this.props.multiRoll(6,this.props.state,this.props.handler)}} className="btn btn-success align-baseline col-1" >d6</button>
+                  <button type="button" onClick={()=>{this.props.multiRoll(4,this.props.state,this.props.handler)}} className="btn btn-success align-baseline col-1" >d4</button>
+                  </div>
+                  <div className="mt-3"></div>
+                  {this.props.EnemyList.map((item,index)=>
+                    <RollEntry key={item.id} data={item} roll={this.props.roll}/>
+                  )}
+            </div>
+        );
+    }
+}
+
+export default multi_roll;
